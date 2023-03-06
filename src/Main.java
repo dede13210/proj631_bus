7a260eb3dd33c09f0385da08d36a3510f1b043bb
Ripsou <48862603+dede13210@users.noreply.github.com>
2023-03-06 16:58:44 +0100

    refactor project in p.o.o

---
+++ src/Main.java
@@ -0,0 +1,35 @@
+import reseaux_bus.Graph;
+import reseaux_bus.Ligne;
+import reseaux_bus.Parser;
+
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String path1 = "file/1_Poisy-ParcDesGlaisins.txt";
+        String path2 = "file/2_Piscine-Patinoire_Campus.txt";
+
+        Ligne ligne1 =new Ligne(null);//on initialise l'objet ligne à null
+        Ligne ligne2= new Ligne(null);
+        ligne1.parseDirection(path1);//on parse la ligne avec le terminus
+        ligne2.parseDirection(path2);
+        Graph reseau1=new Graph();
+        reseau1.parseStop(path1,ligne1);
+        reseau1.parseStop(path2,ligne2);
+        reseau1.setListBusStopVoisin();
+        reseau1.fusionBusStop();
+        reseau1.parseArrete(path1);
+
+
+
+        //test
+        //reseau1.displayGraph();
+        //System.out.println(reseau1.getListBusStop().size());
+        reseau1.displayArete();
+        /*
+        ligne1.displayLigne();
+        System.out.println(" ");
+        ligne2.displayLigne();
+*/
+
+    }
+}
